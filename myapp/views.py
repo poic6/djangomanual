5ed486bd6a7a46d0ba6afebde4a91f6703cc9505
poic6 <bupyong9@nate.com>
2022-09-17 11:03:07 +0900

    20220917-11:03

--- myapp/views.py
+++ myapp/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -25,4 +26,25 @@ def answer_create(request, question_id):
 def question_create(request):
     q = Question(author=request.user, subject=request.POST.get('subject'), content=request.POST.get('content'), create_date=timezone.now())
     q.save()
-    return redirect('list')
+    return redirect('myapp:list')
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'myapp/question_modify.html', context)
+
+@login_required(login_url='common:login')
+def question_modify_go(request):
+    question = get_object_or_404(Question, pk=request.POST.get('id'))
+    question.subject=request.POST.get('subject')
+    question.content=request.POST.get('content')
+    question.modify_date= timezone.now()
+    question.save()
+    return redirect('myapp:list')
+
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.delete()
+    return redirect('myapp:list')
